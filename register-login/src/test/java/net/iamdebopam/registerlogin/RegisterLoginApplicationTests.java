687cda82f19b7b6c152ca47ccf1c629ac6dac324
iamdebopam <iamdebopam8@gmail.com>
2023-03-27 19:56:54 +0530

    API GATEWAY

---
+++ register-login/src/test/java/net/iamdebopam/registerlogin/RegisterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iamdebopam.registerlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
